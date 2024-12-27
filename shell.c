aa8cd5030d98cd1b126b89ebba6cfa3a35921ee5
mung04 <madison.ung@csu.fullerton.edu>
2024-12-27 05:34:47 -0800

    Attempted cd command

--- shell.c
+++ shell.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char* argv[]) {
     int line_length = 1024;
     char command_line[line_length];
+    command_line[strcspn(command_line, "\n")] = '\0';
+    char path[] = "custom_shell";
 
     while (1) {
-        printf("custom_shell> ");
+        printf("%s> ", path);
         fgets(command_line, line_length, stdin);
 
         if (!(strcmp(command_line, "exit\n")) || !(strcmp(command_line, "quit\n"))) {
             printf("exiting custom shell...\n");
             break;
+        }
+
+        // cd
+        if (!(strcmp(command_line, "cd\n"))) {
+            printf("resetting path...\n");
+            strcpy(path, "custom_shell");
+        }
+        else if (strstr(command_line, "cd ") != NULL) {
+            printf("changing directory...\n");
+            char path_sub[line_length];
+            strncpy(path_sub, command_line + 3, strlen(command_line));
+
+            strcat(path, "/");
+            strcat(path, path_sub);
+
+            // if (chdir((char *)path) != 0) {
+            //     perror("cd");
+            // }
         }
 
         printf("Line entered: %s", command_line);
